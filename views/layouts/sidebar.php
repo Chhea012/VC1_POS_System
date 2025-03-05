1d7f40634825601b52348fba09676452d2f75eed
Chhea012 <chhea.chhouy@student.passerellesnumeriques.org>
2025-03-06 01:07:27 +0700

    (refactor): code refactoring no feature no bugs

--- views/layouts/sidebar.php
+++ views/layouts/sidebar.php
@@ -16,7 +16,7 @@
 
     <ul class="menu-inner py-1">
         <!-- Dashboard -->
-        <li class="menu-item active">
+        <li class="menu-item ">
             <a href="./" class="menu-link">
                 <i class="menu-icon tf-icons bx bx-home-circle"></i>
                 <div data-i18n="Analytics">Dashboard</div>
@@ -226,9 +226,6 @@
         </li>
 
         </li>
-
-
-
         <!-- Forms & Tables -->
         <li class="menu-header small text-uppercase"><span class="menu-header-text"> Tables</span></li>
         <!-- Forms -->
