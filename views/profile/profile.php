94db3f58b030ffd00ee00252dd288259cd8ae143
nilrathana <nilrathana.tep@gmail.com>
2025-03-22 12:27:42 +0700

    feat/profile : update some code profile [VC1GS-511]

--- views/profile/profile.php
+++ views/profile/profile.php
@@ -94,6 +94,7 @@ if (!isset($_SESSION['user'])) {
         const file = e.target.files[0];
         if (file) {
             preview.src = URL.createObjectURL(file);
+            preview.style.display = 'block';
         }
     });
 
