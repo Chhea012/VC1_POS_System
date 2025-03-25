bc24544ca574fb5a9441cbe835f70c09a2ad6eef
Seavmey-Yem <seavmey.yem@student.passerellesnumeriques.org>
2025-03-25 13:58:12 +0700

    refactor:style button

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -47,7 +47,7 @@
     <script src="/views/assets/modules/js/config.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     
-
+    
 </head>
 
 <body>
