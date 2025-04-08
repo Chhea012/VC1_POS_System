40c71fc6cd3c3ee650cd7ba786845e8f04511874
Liyaan66 <liya.an@student.passerellesnumeriques.org>
2025-04-08 10:38:19 +0700

    refactor : update layout structure clean code [VC1GS-576]

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -36,6 +36,7 @@
     <link rel="stylesheet" href="/views/assets/modules/vendor/css/theme-default.css" class="template-customizer-theme-css" />
     <link rel="stylesheet" href="/views/assets/modules/css/demo.css" />
     <link rel="stylesheet" href="/views/assets/modules/css/weather.css" />
+    <link rel="stylesheet" href="/views/assets/modules/css/user.css" />
 
     <!-- Vendors CSS -->
     <link rel="stylesheet" href="/views/assets/modules/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
@@ -48,6 +49,7 @@
     <script src="/views/assets/modules/vendor/js/helpers.js"></script>
     <script src="/views/assets/modules/js/config.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" rel="stylesheet">
     
 
 </head>
